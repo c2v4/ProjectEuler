ba4d87f3834f3a2f4a1db38758468c1d5e69dc94
c2v4 <the_pro@wp.pl>
2018-05-08 16:39:51 +0200

    9 Solution

---
+++ src/main/kotlin/Problem 9.kt
@@ -0,0 +1,3 @@
+fun main(args: Array<String>) {
+    print((1..995).asSequence().flatMap { a -> ((a + 1)..997).asSequence().map { b -> Triple(a, b, Math.sqrt(((a * a) + (b * b)).toDouble()).toInt()) }.filter { (a, b, c) -> a * a + b * b == c * c } }.find { (a, b, c) -> a + b + c == 1000 }?.let { (a,b,c) -> a*b*c })
+}
